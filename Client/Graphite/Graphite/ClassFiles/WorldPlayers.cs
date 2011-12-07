a3ca717bdedead4640d8a16b7137b57e6b027e4c
Jonathan <JonJpGardner@gmail.com>
2011-12-07 12:11:21 -0400

    Fixin this shit hole
    Signed-off-by: Jonathan <JonJpGardner@gmail.com>

--- Client/Graphite/Graphite/ClassFiles/WorldPlayers.cs
+++ Client/Graphite/Graphite/ClassFiles/WorldPlayers.cs
@@ -2,10 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 //XNA
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
@@ -29,37 +25,39 @@ namespace Graphite
 
         //Non-Player Data
         public int[] WeaponIDs;
+        public int[] HatIDs;
 
         //Variables
         //Player (sent to server)
         public Vector2 playerLoc;
         public Vector2 pMouseLoc;
-        string strUserName;
-        int intUserID;
-        int intTeamID;
-        bool blnFiring = false;
+        public string strUserName = "TEST";  //unless set by Login
+        public int intUserID = 1;            //unless set by Login
+        public int intTeamID = 1;            //unless set by Login
+        public bool blnFiring = false;
+        public int intCurrentWeapID;
+        public int intHatID;
+        int intPlayerWeight;
 
-        int intCurrentWeapID;
-        int[] intWeapSlot = { 0, 0, 0 };
-        int intHatID;
-        int intExp;
-        int intMoney;
         //Player (not sent to server)
-        Vector2 moveSpeed;
-
+        Vector2 moveSpeed;        
+        public int[] intWeapSlot = { 0, 0, 0 };
+        public int intExp;
+        public int intMoney;
         //Sub Class Files
         TCPServer_Class TCPConnect = new TCPServer_Class();
 
         //Load On Game Start
         public void Initialize()
         {
-
+            TCPConnect.Connect();
+            TCPConnect.Threading();
         }
         //Update Player Per Tick
         public void Update()
         {
             KeyboardState keystate = Keyboard.GetState();
-
+                                                                        // Change For Weight System
             if (keystate.IsKeyDown(Keys.LeftControl)) { moveSpeed -= new Vector2(2, 2); } //Crouching
             else if (keystate.IsKeyDown(Keys.LeftShift)) { moveSpeed += new Vector2(2, 2); }//Sprinting
 
@@ -70,47 +68,26 @@ namespace Graphite
             if (keystate.IsKeyDown(Keys.S)) { playerLoc.Y += moveSpeed.Y; }
             if (keystate.IsKeyDown(Keys.A)) { playerLoc.X -= moveSpeed.X; }
             if (keystate.IsKeyDown(Keys.D)) { playerLoc.X += moveSpeed.X; }
+            //TEMP
+            if (keystate.IsKeyDown(Keys.Escape)) { Environment.Exit(0); }
 
-
-
-            //Change Weap F1-F2-F3-Scroll Wheel
-
+            //Mouse
             MouseState mousestate = Mouse.GetState();
-
-
-
+            pMouseLoc = new Vector2(mousestate.X, mousestate.Y);
 
             if (mousestate.LeftButton == ButtonState.Pressed)
             {
                 blnFiring = true;
             }
+            else { blnFiring = false;}
 
-            //SEND SERVER DATA (add playerWeight)
-            TCPConnect.SendServerData(strUserName, intUserID, Convert.ToInt32(playerLoc.X), Convert.ToInt32(playerLoc.Y),
-            Convert.ToInt32(pMouseLoc.X), Convert.ToInt32(pMouseLoc.Y), blnFiring, intCurrentWeapID);
+            //Change Weap 1-2-3-Scroll Wheel
 
-            // GET SERVER DATA            
-        }
-        //Logged Into MySQL
+                //SEND SERVER DATA (add )
+                TCPConnect.SendServerData(strUserName, intUserID, intTeamID, Convert.ToInt32(playerLoc.X), Convert.ToInt32(playerLoc.Y),
+                Convert.ToInt32(pMouseLoc.X), Convert.ToInt32(pMouseLoc.Y), blnFiring, intCurrentWeapID, intHatID, intPlayerWeight);
 
-        public void OnLoggedIn(string UserName, int UserID, int TeamID, Vector2 LastLocation,
-                    int WeapSlot1, int WeapSlot2, int WeapSlot3, int HatID, int Money, int Exp)
-        {
-            //Update Player Variables From MySQL Database
-            playerLoc = LastLocation;
-            strUserName = UserName;
-            intUserID = UserID;
-            intTeamID = TeamID;
-            intWeapSlot[1] = WeapSlot1;
-            intWeapSlot[2] = WeapSlot2;
-            intWeapSlot[3] = WeapSlot3;
-            intHatID = HatID;
-            intMoney = Money;
-            intExp = Exp;
-
-            // After User Data is Retrived Connect to RelayServer
-            TCPConnect.Connect();
-            TCPConnect.Threading();
+            // GET SERVER DATA
         }
         //Update Player MySQL
         public void UpdatePlayerBase()
